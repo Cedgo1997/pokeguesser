77b96fd23cf21bfb9f80aa3969d2f19d81ac5b6c
cesarisim0 <cedgo1997@gmail.com>
2024-02-22 06:24:01 -0400

    Fetching pokemon list with axios and pokeapi

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,15 +1,26 @@
+import pokemonApi from "@/api/pokemonApi";
+
 const getPokemonList = () => {
-    const pokemonOptions = new Array();
+    const pokemonOptions = Array.from(Array(650));
     return pokemonOptions.map((_, index) => index + 1);
 };
 
-const getPokemonOptions = () => {
+const getPokemonOptions = async () => {
     const mixedPokemon = getPokemonList().sort(() => Math.random() - 0.5);
-    getPokemonNames(mixedPokemon.splice(0, 4));
+    const pokemonNames = await getPokemonNames(mixedPokemon.splice(0, 4));
+    return pokemonNames
 };
 
-const getPokemonNames = (pokemonList = []) => {
-    console.log(pokemonList);
+const getPokemonNames = async (pokemonList = []) => {
+    const promises = pokemonList.map((pokemon) =>
+        pokemonApi.get(`/${pokemon}`)
+    );
+
+    const pokemonData = await Promise.all(promises);
+    return pokemonData.map(({ data }) => ({
+        id: data.id,
+        name: data.name,
+    }));
 };
 
 export default getPokemonOptions;
